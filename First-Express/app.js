f804e6e3c7c3415faee399e0e912e0650d2a6c4d
Kyle <37756806+kjm1550@users.noreply.github.com>
2019-06-05 21:57:13 -0500

    Added a catch all and used params from the url
    Added a catch all for unknown urls to display a page. Used the request paramiters in the url in the page that came up.

--- First-Express/app.js
+++ First-Express/app.js
@@ -19,6 +19,19 @@ app.get('/dog', function(req, res) {
 	console.log('request to /dog');
 });
 
+//
+app.get('/r/:subredditName', function(req, res) {
+	let subreddit = req.params.subredditName;
+	res.send('Welcome to the ' + subreddit + ' subreddit');
+	console.log('request to /r/ ' + subreddit);
+});
+
+// a star or catch all for routes not specified above
+app.get('*', function(req, res) {
+	res.send('You are a star!');
+	res.send('404 page not found!');
+});
+
 // tell express to listen for request
 app.listen(5500, '127.0.0.1', function() {
 	console.log('server has started!');
