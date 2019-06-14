00e7b6ac738baec81a5debda34e77dc0d65bb732
Kyle <37756806+kjm1550@users.noreply.github.com>
2019-06-13 21:25:44 -0500

    Created partials for a Header and Footer
    Created partials for a header and footer. Linked a style sheet to all of the pages.

--- EJSDemo/app.js
+++ EJSDemo/app.js
@@ -1,20 +1,23 @@
 let express = require('express');
 let app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-	res.render('home.ejs');
+	res.render('home');
 });
 
 app.get('/fallinlovewith/:thing', function(req, res) {
 	let thing = req.params.thing;
 
-	res.render('love.ejs', { thingVar: thing });
+	res.render('love', { thingVar: thing });
 });
 
 app.get('/posts', function(req, res) {
 	let posts = [{ title: 'Post1', author: 'Bob' }, { title: 'Post2', author: 'Rob' }, { title: 'Post3', author: 'Jim' }];
 
-	res.render('posts.ejs', { posts: posts });
+	res.render('posts', { posts: posts });
 });
 
 app.listen(5500, '127.0.0.1', function() {
